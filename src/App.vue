1b40524dae2ef47fabfd5dc501c515888a7a571a
lin <mac@lesuobingdu.local>
2020-12-03 20:45:35 +0800

    second time

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <h1>heima</h1>
+    <!-- 路由出口 -->
+    <router-view/>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style lang="less"></style>
